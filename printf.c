f58dff6badeadc712798a94e4134161e291662ff
LizethVictoria20 <lizvictoria754@gmail.com>
2020-03-16 21:59:51 +0000

    Function that print formats s and c

--- printf.c
+++ printf.c
@@ -11,39 +11,51 @@
 
 int _printf(const char *format, ...)
 {
-		va_list args;
+    va_list args;
 
-		int i;
+    int i;
 
-		va_start(args, format);
+    va_start(args, format);
+    if (format == " ")
+    {
+        va_end(args);
+        return (0);
+    }
+    if (format == NULL)
+    {
+        _printf("(null)");
+        va_end(args);
+        return (0);
+    }
+    for (i = 0; format[i] != '\0'; i++)
+    {
+        if (format[i] != '%')
+        {
+            _putchar(format[i]);
+        }
+        else
+        {
+            char nextLetter = format[i + 1];
 
-		for (i = 0; format[i] != '\0'; i++)
-		{
-			if (format[i] != '%')
-			{
-				_putchar(format[i]);
-			}
-			else
-			{
-				char nextLetter = format[i + 1];
+            if (nextLetter == 'c')
+            {
+                char parametroChar = va_arg(args, char*);
 
-				if (nextLetter == 'c')
-				{
-					char *parametro = va_arg(args, char*);
+                _putchar(parametroChar);
+                i++;
+            }
 
-					_putchar(parametro[0]);
-					i++;
-				}
-				else if (nextLetter == 's')
-				{
-					char *parametro = va_arg(args, char*);
+            else if (nextLetter == 's')
+            {
+                char *parametro = va_arg(args, char*);
 
-					_printf(parametro, 0);
-					i++;
-				}
-			}
-		}
+                _printf(parametro, 0);
+                i++;
+            }
 
-	va_end(args);
-	return (i);
+        }
+    }
+
+    va_end(args);
+    return (i);
 }
