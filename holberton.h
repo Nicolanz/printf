5da2fdc41fffe026879c7439d274f4dcb0bd662d
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-03-17 02:30:52 +0000

    Punto 0 y 1

---
+++ holberton.h
@@ -0,0 +1,6 @@
+#ifndef _SAMPLE_HEADER_H_
+#define _SAMPLE_HEADER_H_
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+#endif /*_SAMPLE_HEADER_H_*/
---
+++ printf.c
@@ -0,0 +1,67 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * printnumber - prints ints
+ * @n: Number
+ *
+ */
+void printnumber(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * (-1);
+	}
+	if (n / 10)
+	{
+		printnumber(n / 10);
+	}
+	_putchar(n % 10 + '0');
+}
+/**
+ * _printf - Function printf().
+ * @format: Pointer
+ * Return: Always 0.
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+
+	int i;
+  
+	va_start(args, format);
+    
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{  
+			_putchar(format[i]);
+		}
+		else
+		{
+			char siguienteLetra = format[i + 1];
+			if (siguienteLetra == 'c')
+			{
+				char *parametro = va_arg(args, char*);
+				_putchar(parametro[0]);
+				i++;
+			}
+			else if (siguienteLetra == 's')
+			{
+				char *parametro = va_arg(args, char*);
+				_printf(parametro, 0);
+				i++;
+			}
+			else if (siguienteLetra == 'i' || siguienteLetra == 'd')
+			{
+				int num = va_arg(args, int);
+				printnumber(num);
+				i++;
+			}
+		}
+	}
+	_putchar('\n');
+	return (i);
+}
